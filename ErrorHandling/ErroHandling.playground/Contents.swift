24f66cca7ba8c8ca8c3b0ec34bbaac8adc2fd6c4
Mac <mac@Macs-MacBook-Air.local>
2018-02-21 00:42:10 +0530

    Error complete method

--- ErrorHandling/ErroHandling.playground/Contents.swift
+++ ErrorHandling/ErroHandling.playground/Contents.swift
@@ -4,3 +4,92 @@ import UIKit
 
 var str = "Hello, playground"
 
+/* to indicate that function method, or  initializer can throw an error, you write a __throws__ keyword
+ * a function marked with trows is called throwing function
+ */
+
+/// example ---- func canThrowError() throws -> String {}
+/// example ---- func cannotThrowError() -> String {}
+
+enum VendingMachinError: Error {
+    case invalidSelection
+    case insufficientFund(coinsNeeded: Int)
+    case outOfStock
+}
+
+
+struct Item {
+    var price: Int
+    var count: Int
+}
+
+class VendingMachin {
+    var inventory = [  "Candy Bar": Item(price: 12, count: 7),
+        "Chips": Item(price: 10, count: 4),
+        "Pretzels": Item(price: 7, count: 11)
+    ]
+    
+    var coinDeposited = 0
+    
+    func vend(itemNamed name: String) throws {
+        
+        guard let item = inventory[name] else {
+            throw VendingMachinError.invalidSelection
+        }
+        
+        guard item.count > 0 else {
+            throw VendingMachinError.outOfStock
+        }
+        
+        guard item.price <= coinDeposited else {
+            throw VendingMachinError.insufficientFund(coinsNeeded: item.price - coinDeposited)
+        }
+        
+        coinDeposited -= item.price
+        
+        var newItem = item
+        newItem.count -= 1
+        inventory[name] = newItem
+        
+        print("Discription \(inventory[name])")
+        
+    }
+}
+
+
+let favouriteSnacks = [ "Alice": "Chips", 
+                    "Bob": "Licorice",
+                    "Eve": "Pretzels"
+                ]
+
+func buyFavouriteSnacks(person: String, vendingMachine: VendingMachin) throws {
+    let snackNames = favouriteSnacks[person] ?? "Candy Bar"
+    try vendingMachine.vend(itemNamed: snackNames)
+}
+
+
+struct PurchasSnack {
+    let  name: String
+    init(name: String, vendingMachin: VendingMachin) throws {
+        try vendingMachin.vend(itemNamed: name)
+        self.name = name
+    }
+}
+
+var vendingMachine = VendingMachin()
+vendingMachine.coinDeposited = 8
+do {
+    try buyFavouriteSnacks(person: "Alice", vendingMachine: vendingMachine)
+    print("Success! Yum.")
+} catch VendingMachinError.invalidSelection {
+    print("Invalid Selection.")
+} catch VendingMachinError.outOfStock {
+    print("Out of Stock.")
+} catch VendingMachinError.insufficientFund(let coinsNeeded) {
+    print("Insufficient funds. Please insert an additional \(coinsNeeded) coins.")
+} catch {
+    print("Unexpected error: \(error).")
+}
+
+
+
